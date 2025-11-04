e92e24d8e2cb845c89d415eb8fa74efc978f4faf
Dracxvil <dracxvilhelphraenyx@gmail.com>
2025-11-04 12:44:14 +0530

    TwitchConnect using OAuth Token(Temporary)

---
+++ Assets/Managers/TwitchConnect.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+
+
+public class TwitchConnect : MonoBehaviour
+{
+    TcpClient Twitch;
+    StreamReader Reader;
+    StreamWriter Writer;
+
+    const string URL = "irc.chat.twitch.tv";
+    const int PORT = 6667;
+
+    string User = "Dracxvil";
+    //Get OAuth from https://twitchapps.com.tmi
+    string OAuth = "oauth:36363pesiy08cudclwdanlmp3pvgct";
+    string Channel = "Dracxvil";
+
+    private void ConnectToTwitch()
+    {
+        Twitch = new TcpClient(URL, PORT);
+        Reader= new StreamReader(Twitch.GetStream());
+        Writer = new StreamWriter(Twitch.GetStream());
+
+        Writer.WriteLine("PASS " + OAuth);
+        Writer.WriteLine("NICK " + User.ToLower());
+        Writer.WriteLine("JOIN #" + Channel.ToLower());
+        Writer.Flush();
+    }
+
+    private void Awake()
+    {
+        ConnectToTwitch();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Twitch.Available > 0)
+        {
+            string message = Reader.ReadLine();
+
+            print(message);
+        }
+    }
+}
